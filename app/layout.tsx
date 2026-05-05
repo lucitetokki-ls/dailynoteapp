96ed5392b1093769d4423d1380b3a674752a0a68
lucitetokki <inaeo@kakao.com>
2026-05-06 02:32:36 +0900

    Fix D2Coding font loading on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/gh/joungkyun/font-d2coding@1.3.2/d2coding.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
